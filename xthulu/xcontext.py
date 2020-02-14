bd600bd311ec505d727f156122e08d8a6c25dfc5
haliphax <haliphax@gmail.com>
2020-02-14 01:52:16 -0600

    context logger with username/ip

--- xthulu/xcontext.py
+++ xthulu/xcontext.py
@@ -2,47 +2,46 @@
 
 # stdlib
 from collections import namedtuple
+import logging
+import sys
 # local
 from . import log as syslog
 from .events import EventQueues
 from .exceptions import Goto, ProcessClosing
-from .structs import EventData, Script
+from .structs import Script
 
 
 class XthuluContext(object):
 
     "Context object for SSH sessions"
 
-    #: SSHServerProcess
-    proc = None
-    #: AsyncTerminal
-    term = None
     #: Script stack
     stack = []
-    #: Event queue for this session
-    events = []
-    #: Encoding for connection
-    encoding = 'utf-8'
 
-    def __init__(self, proc, *args, **kwargs):
+    def __init__(self, proc, encoding='utf-8'):
+        #: SSHServerProcess for session
         self.proc = proc
+        #: Encoding for session
+        self.encoding = encoding
+        #: Username
+        self.username = proc.get_extra_info('username')
+        #: Remote IP address
+        self.ip = proc.get_extra_info('peername')[0]
+        #: Logging facility
+        self.log = logging.getLogger('xc')
+        #: Events queue
         self.events = EventQueues.q['{}:{}'
                 .format(*self.proc.get_extra_info('peername'))]
 
-        for k in kwargs.keys():
-            setattr(self, k, kwargs[k])
-
-    @property
-    def username(self):
-        "Client username"
-
-        return self.proc.get_extra_info('username')
-
-    @property
-    def remote_ip(self):
-        "Remote IP address of client"
-
-        return self.proc.get_extra_info('peername')[0]
+        # set up logging
+        self.log.addFilter(XthuluContextLogFilter(self.username, self.ip))
+        streamHandler = logging.StreamHandler(sys.stdout)
+        streamHandler.setFormatter(logging.Formatter(
+                '{asctime} {levelname} {module}.{funcName}: {username}@{ip} '
+                '{message}',
+                style='{'))
+        self.log.addHandler(streamHandler)
+        self.log.setLevel(syslog.getEffectiveLevel())
 
     def echo(self, text):
         "Echo text to the terminal"
@@ -52,7 +51,7 @@ class XthuluContext(object):
     async def gosub(self, script, *args, **kwargs):
         "Execute script and return result"
 
-        script = Script(name=script, args=args, kwargs=kwargs)
+        script = Script(script, args, kwargs)
 
         return await self.runscript(script)
 
@@ -61,16 +60,10 @@ class XthuluContext(object):
 
         raise Goto(script, *args, **kwargs)
 
-    def log(self, level, message):
-        "Log message with username@host"
-
-        func = getattr(syslog, level)
-        func('{}@{} {}'.format(self.username, self.remote_ip, message))
-
     async def runscript(self, script):
         "Run script and return result; used by :meth:`goto` and :meth:`gosub`"
 
-        self.log('info', 'Running {}'.format(script))
+        self.log.info('Running {}'.format(script))
         imp = __import__('scripts', fromlist=(script.name,))
 
         try:
@@ -78,7 +71,23 @@ class XthuluContext(object):
                                                         **script.kwargs)
         except (ProcessClosing, Goto):
             raise
-        # except Exception as exc:
-        #     self.echo(self.term.bright_red_on_black('Exception in {}\n'
-        #                                             .format(script.name)))
-        #     self.log('exception', exc)
+        except Exception as exc:
+            self.echo(self.term.bright_red_on_black('\r\nException in {}\r\n'
+                                                    .format(script.name)))
+            self.log.exception(exc)
+
+
+class XthuluContextLogFilter(logging.Filter):
+
+    "Custom logging.Filter that injects username and remote IP address"
+
+    def __init__(self, username, ip):
+        self.username = username
+        self.ip = ip
+
+    def filter(self, record):
+        "Filter log record"
+
+        record.username = self.username
+        record.ip = self.ip
+        return True
