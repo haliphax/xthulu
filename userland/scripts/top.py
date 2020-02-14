a1a92fb12d88a1b8c6b309e8a7440b095f550a29
haliphax <haliphax@gmail.com>
2020-02-14 00:48:47 -0600

    add encoding handshake to top, use \r\n

--- userland/scripts/top.py
+++ userland/scripts/top.py
@@ -1,10 +1,22 @@
 "Userland entry point"
 
+from xthulu import log
+
 async def main(xc):
     xc.echo(xc.term.normal)
-    xc.echo('Connected: {}@{}\n'
+    xc.echo('Connected: {}@{}\r\n'
             .format(xc.term.bright_blue(xc.username),
                     xc.term.bright_blue(xc.remote_ip)))
+
+    if xc.encoding == 'utf-8':
+        xc.echo('\x1b%G')
+    else:
+        xc.echo('\x1b%@\x1b(U')
+
+    xc.echo(xc.term.move_x(0) + xc.term.clear_eol)
+
+    for k in xc.proc.env.keys():
+        xc.echo('{} = {}\r\n'.format(k, xc.proc.env[k]))
 
     while True:
         ks = None
@@ -14,20 +26,20 @@ async def main(xc):
                 ev = xc.events.pop(0)
 
                 if ev.name == 'resize':
-                    xc.echo(xc.term.bright_green('RESIZE!\n'))
+                    xc.echo(xc.term.bright_green('RESIZE!\r\n'))
 
             ks = await xc.term.inkey(0.1)
 
         if ks.code == xc.term.KEY_LEFT:
-            xc.echo(xc.term.bright_red('LEFT!\n'))
+            xc.echo(xc.term.bright_red('LEFT!\r\n'))
         elif ks.code == xc.term.KEY_RIGHT:
-            xc.echo(xc.term.bright_red('RIGHT!\n'))
+            xc.echo(xc.term.bright_red('RIGHT!\r\n'))
         elif ks.code == xc.term.KEY_UP:
-            xc.echo(xc.term.bright_red('UP!\n'))
-            xc.echo('{}\n'.format(await xc.gosub('retval')))
+            xc.echo(xc.term.bright_red('UP!\r\n'))
+            xc.echo('{}\r\n'.format(await xc.gosub('retval')))
         elif ks.code == xc.term.KEY_DOWN:
-            xc.echo(xc.term.bright_red('DOWN!\n'))
+            xc.echo(xc.term.bright_red('DOWN!\r\n'))
             await xc.gosub('down', 1, arg2='adsf')
         elif ks.code == xc.term.KEY_ESCAPE:
-            xc.echo(xc.term.bright_red('ESCAPE!\n'))
+            xc.echo(xc.term.bright_red('ESCAPE!\r\n'))
             xc.proc.exit(0)
