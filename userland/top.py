9858073e4468416c4c697ddff1e9b1685e12c71c
haliphax <haliphax@gmail.com>
2020-03-05 16:44:55 -0600

    editor colors, refresh -> redraw, redraw_cursor, etc.

--- userland/top.py
+++ userland/top.py
@@ -13,11 +13,11 @@ async def main(cx):
     cx.echo(cx.term.normal)
     cx.echo(cx.term.move_x(0) + cx.term.clear_eol)
     cx.echo('Connected: {}@{}\r\n'
-            .format(cx.term.bold_blue(cx.username),
+            .format(cx.term.bold_blue(cx.user.name),
                     cx.term.bold_blue(cx.ip)))
 
-    led = LineEditor(cx.term, cx.term.width - 1, x=cx.term.height, y=0)
-    led.text = 'testing this thing'
+    led = LineEditor(cx.term, cx.term.width - 1, color='bold_white_on_green',
+                     value='testing this thing')
 
     for k in cx.env.keys():
         cx.echo('{} = {}\r\n'.format(k, cx.env[k]))
@@ -26,7 +26,7 @@ async def main(cx):
 
     while True:
         if dirty:
-            cx.echo(cx.term.move_x(0) + led.refresh())
+            cx.echo(cx.term.move_x(0) + led.redraw())
             dirty = False
 
         ks = None
--- xthulu/ui/editors.py
+++ xthulu/ui/editors.py
@@ -5,12 +5,16 @@ class BlockEditor(object):
 
     "Block editor (multiple lines)"
 
+    #: Editor text
+    value = ''
     #: Text length limit
     limit = 0
-    #: Text
-    text = ''
     #: Cursor position
     pos = (0, 0)
+
+    # internals
+    _color_str = 'bold_white_on_blue'
+    _color = None
 
     def __init__(self, term, rows, width, **kwargs):
         #: Terminal to use for sequences
@@ -23,50 +27,71 @@ class BlockEditor(object):
         for k in kwargs.keys():
             setattr(self, k, kwargs[k])
 
-    def refresh(self):
+        self._color = getattr(term, self._color_str)
+
+    @property
+    def color(self):
+        "Color property; setting it also sets the internal Terminal callable"
+
+        return getattr(self.term, self._color_str)
+
+    @color.setter
+    def color(self, val):
+        self._color_str = val
+        self._color = getattr(self.term, val)
+
+    def redraw(self):
         "Output sequence to redraw editor"
 
         out = ''
-        split = self.text.split('\r\n')
+        split = self.value.split('\r\n')
         txtlen = len(split)
 
         for i in range(self.rows):
             if i > 0:
                 out += '\r\n'
 
-            out += self.term.on_blue(' ' * self.width)
+            out += self._color(' ' * self.width)
 
             if i < txtlen:
                 out += self.term.move_left() * self.width
-                out += self.term.bold_white_on_blue(
-                        split[i][-self.width:])
+                out += self._color(split[i][-self.width:])
 
         return out
+
+    def redraw_cursor(self):
+        """
+        Output sequence to restore cursor position; assumes cursor is already
+        located at top-left of editor
+        """
+
+        return (self.term.move_down(self.pos[0]) +
+                self.term.move_right(self.pos[1]))
 
     def process_keystroke(self, ks):
         "Process keystroke and produce output (if any)"
 
-        if ks.code == self.term.KEY_BACKSPACE and len(self.text):
-            self.text = self.text[:-1]
+        if ks.code == self.term.KEY_BACKSPACE and len(self.value):
+            self.value = self.value[:-1]
 
-            return (self.term.move_left() + self.term.bold_white_on_blue(' ')
+            return (self.term.move_left() + self._color(' ')
                     + self.term.move_left())
 
         if ks.is_sequence:
             return
 
         ucs = str(ks)
-        self.text += ucs
+        self.value += ucs
 
-        return self.term.bold_white_on_blue(ucs)
+        return self._color(ucs)
 
 
 class LineEditor(BlockEditor):
 
     "Line editor (single line)"
 
-    def __init__(self, term, width, x=None, y=None):
-        super().__init__(term, 1, width, x=x, y=y)
+    def __init__(self, term, width, *args, **kwargs):
+        super().__init__(term, 1, width, *args, **kwargs)
 
     @property
     def rows(self):
@@ -77,5 +102,3 @@ class LineEditor(BlockEditor):
     def rows(self, val):
         # immutable
         pass
-
-    rows.__doc__ = BlockEditor.rows.__doc__
