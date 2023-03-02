c5a1c804dd480e681443f8601534b254d38d59dc
haliphax <haliphax@gmail.com>
2023-03-02 12:17:37 -0600

    add some flair to the demo scripts

--- userland/scripts/lock_example.py
+++ userland/scripts/lock_example.py
@@ -1,14 +1,37 @@
 """Lock example"""
 
+# stdlib
+from asyncio import sleep
+
 # api
 from xthulu.ssh.context import SSHContext
 
 
 async def main(cx: SSHContext):
+    cx.echo(
+        cx.term.normal,
+        "\r\n\r\n",
+        cx.term.bold_white_on_yellow_underline(" Shared locks demo "),
+        "\r\n\r\n",
+    )
+
     with cx.lock("testing") as l:
-        if not l:
-            cx.echo("Unable to acquire lock\r\n")
-        else:
-            cx.echo("Got lock\r\n")
+        if l:
+            if cx.encoding == "utf-8":
+                cx.echo("🔒 ")
+
+            cx.echo("Lock acquired; press any key to release\r\n")
             await cx.term.inkey()
-            cx.echo("Released\r\n")
+
+            if cx.encoding == "utf-8":
+                cx.echo("🔥 ")
+
+            cx.echo("Lock released!\r\n")
+            await sleep(1)
+
+            return
+
+    if cx.encoding == "utf-8":
+        cx.echo("❌ ")
+
+    cx.echo("Failed to acquire lock\r\n")
