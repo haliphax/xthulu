de97edccc743975c6289192fad4095caf37a2fd5
haliphax <haliphax@gmail.com>
2023-10-12 00:41:57 -0500

    🧑‍💻 fix dev containers (sort of)

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -16,12 +16,13 @@ curl -L \
 cat >> /home/$USER/.bashrc <<EOF
 PYENV_ROOT="\$HOME/.pyenv"
 command -v pyenv >/dev/null || PATH="\$PYENV_ROOT/bin:\$PATH"
+PATH="\$PYENV_ROOT/versions/xthulu/bin:\$PATH"
 eval "\$(pyenv init -)"
 eval "\$(pyenv virtualenv-init -)"
 EOF
 
 PYENV_ROOT="$HOME/.pyenv"
-PATH="$PYENV_ROOT/bin:$PATH"
+PATH="$PYENV_ROOT/versions/xthulu/bin:$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 pyenv install 3.11
@@ -31,8 +32,9 @@ pyenv virtualenv 3.11 xthulu
 echo xthulu > .python-version
 cp .devcontainer/docker-compose.override.yml .
 pip install -Ue .[dev,hiredis]
+nodeenv -p
 npm ci
 pre-commit install --install-hooks
 etc/gitmoji-hook.sh
+cd docker || exit 1
 docker compose build
-docker compose pull
