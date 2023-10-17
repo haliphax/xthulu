2ea020adaff8a1349e4e560bfaaa724b35e18f46
haliphax <haliphax@gmail.com>
2023-10-16 19:18:13 -0500

    ♻️ load artwork in BannerApp init

--- xthulu/ssh/console/banner_app.py
+++ xthulu/ssh/console/banner_app.py
@@ -33,6 +33,7 @@ class BannerApp(XthuluApp):
     def __init__(
         self, context: SSHContext, art_path: str, art_encoding: str, **kwargs
     ):
+        self.art_encoding = art_encoding
         self.art_path = art_path
         self.artwork = []
         super().__init__(context=context, **kwargs)
@@ -63,7 +64,7 @@ class BannerApp(XthuluApp):
         yield banner
 
     async def on_ready(self):
-        self.artwork = await load_art("userland/artwork/oneliners.ans", "amiga")
+        self.artwork = await load_art(self.art_path, self.art_encoding)
         self._update_banner()
 
     def on_resize(self, event: events.Resize) -> None:
