c8cf097210b8016c300a513de6f82c1c5b7d3f9d
haliphax <haliphax@gmail.com>
2023-10-19 13:41:17 -0500

    ♻️ use dataclass instead of namedtuple

--- xthulu/ssh/structs.py
+++ xthulu/ssh/structs.py
@@ -1,14 +1,20 @@
 """SSH server structs"""
 
 # stdlib
-from collections import namedtuple
+from dataclasses import dataclass
+from typing import Any, Sequence
 
-Script = namedtuple(
-    "Script",
-    (
-        "name",
-        "args",
-        "kwargs",
-    ),
-)
-"""A userland Python script"""
+
+@dataclass
+class Script:
+
+    """A userland Python script"""
+
+    name: str
+    """The script name, used for lookup"""
+
+    args: Sequence[Any]
+    """The script's `*args` list"""
+
+    kwargs: dict[str, Any]
+    """The scripts `**kwargs` dict"""
