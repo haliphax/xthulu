a09107a3481c69d5e980dcaee6fa31756409687a
haliphax <haliphax@gmail.com>
2024-03-26 10:27:54 -0500

    💄 better message editor modal css

--- userland/scripts/messages/save_screen.py
+++ userland/scripts/messages/save_screen.py
@@ -11,9 +11,26 @@ class SaveScreen(ModalScreen):
     """Save confirmation screen"""
 
     CSS = """
+        SaveScreen {
+            align: center middle;
+            background: rgba(0, 0, 0, 0.5);
+        }
+
         Button {
             margin: 1;
             width: 33%;
+        }
+
+        #save {
+            margin-left: 0;
+            margin-top: 1;
+        }
+
+        #wrapper {
+            background: $primary-background;
+            height: 7;
+            padding: 1;
+            width: 60;
         }
     """
 
@@ -27,6 +44,7 @@ class SaveScreen(ModalScreen):
                 Button("Continue", variant="primary", id="continue"),
                 Button("Discard", variant="error", id="discard"),
             ),
+            id="wrapper",
         )
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
