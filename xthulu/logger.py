9ef3f44033a5eb1e0eafe3dd7ff399bc44b0ad3d
haliphax <haliphax@gmail.com>
2024-03-28 15:18:18 -0500

    ✨ log to file

--- xthulu/logger.py
+++ xthulu/logger.py
@@ -2,6 +2,7 @@
 
 # stdlib
 from logging import DEBUG, Formatter, getLogger, INFO, StreamHandler
+from logging.handlers import TimedRotatingFileHandler
 from sys import stdout
 
 # local
@@ -10,11 +11,19 @@ from .configuration import get_config
 log = getLogger(__name__)
 """Root logger instance"""
 
-streamHandler = StreamHandler(stdout)
-streamHandler.setFormatter(
-    Formatter(
-        "{asctime} {levelname:<7} <{module}.{funcName}> {message}", style="{"
-    )
+formatter = Formatter(
+    "{asctime} {levelname:<7} <{module}.{funcName}> {message}", style="{"
 )
+"""LogRecord formatter"""
+
+fileHandler = TimedRotatingFileHandler("run/logs/xthulu.log", when="d")
+"""Rotating file handler"""
+
+streamHandler = StreamHandler(stdout)
+"""Console stream handler"""
+
+fileHandler.setFormatter(formatter)
+streamHandler.setFormatter(formatter)
+log.addHandler(fileHandler)
 log.addHandler(streamHandler)
 log.setLevel(DEBUG if bool(get_config("debug.enabled", False)) else INFO)
