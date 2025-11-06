f9b922b04980f89775c1c1a9feb451e8fca83b22
haliphax <haliphax@gmail.com>
2025-11-05 22:53:17 -0600

    💄 simple SelectionList for message tagging

--- userland/scripts/messages/details_modal.py
+++ userland/scripts/messages/details_modal.py
@@ -1,11 +1,13 @@
 """Message details screen"""
 
 # 3rd party
-from sqlmodel import col, select
+from typing import Sequence
+from sqlmodel import and_, col, select
 from textual.containers import Horizontal, Vertical
 from textual.screen import ModalScreen
 from textual import validation
-from textual.widgets import Button, Input, Label, TextArea
+from textual.widgets import Button, Input, Label, SelectionList, TextArea
+from textual.widgets.selection_list import Selection
 
 # api
 from xthulu.resources import db_session
@@ -28,16 +30,20 @@ class DetailsModal(ModalScreen):
 
         Label {
             margin-top: 1;
-            width: 5;
+            width: 5
+        }
+
+        Input {
+            width: 53;
+        }
+
+        SelectionList {
+            height: 10;
         }
 
         Button {
             margin-top: 1;
             width: 50%;
-        }
-
-        Input {
-            width: 53;
         }
 
         #save {
@@ -47,7 +53,7 @@ class DetailsModal(ModalScreen):
 
         #wrapper {
             background: $primary-background;
-            height: 12;
+            height: 20;
             padding: 1;
             width: 60;
         }
@@ -80,10 +86,7 @@ class DetailsModal(ModalScreen):
                     validate_on=("changed", "submitted"),
                 ),
             ),
-            Horizontal(
-                Label("Tags", shrink=True),
-                Input(id="tags"),
-            ),
+            SelectionList(id="tags"),
             Horizontal(
                 Button("Save", variant="success", name="save", id="save"),
                 Button("Cancel", variant="error", name="cancel"),
@@ -92,21 +95,33 @@ class DetailsModal(ModalScreen):
         )
 
     async def on_mount(self) -> None:
-        if not self.reply_to:
-            return
+        tags: SelectionList = self.get_widget_by_id("tags")  # type: ignore
 
         async with db_session() as db:
-            tag_results = (
+            if not self.reply_to:
+                my_tags = []
+            else:
+                my_tags = (
+                    await db.exec(
+                        select(MessageTags.tag_name).where(
+                            MessageTags.message_id == self.reply_to.id
+                        )
+                    )
+                ).all()
+
+            all_tags: Sequence[str] = (  # type: ignore
                 await db.exec(
-                    select(MessageTags).where(
-                        MessageTags.message_id == self.reply_to.id
+                    select(MessageTag.name).where(
+                        and_(
+                            col(MessageTag.name).is_not(None),
+                            col(MessageTag.name).not_in(my_tags),
+                        )
                     )
                 )
             ).all()
 
-        tags = " ".join([t.tag_name for t in tag_results])
-        inp: Input = self.get_widget_by_id("tags")  # type: ignore
-        inp.value = tags
+        tags.add_options([Selection(t, t, True) for t in my_tags])
+        tags.add_options([Selection(t, t, False) for t in all_tags])
 
     async def submit(self) -> None:
         app: XthuluApp = self.app  # type: ignore
@@ -117,29 +132,10 @@ class DetailsModal(ModalScreen):
             return
 
         content = self.app._background_screens[-1].query_one(TextArea)
-        tags: Input = self.get_widget_by_id("tags")  # type: ignore
-        tags_validator = tags.validate(tags.value)
+        tags: SelectionList = self.get_widget_by_id("tags")  # type: ignore
 
-        if tags_validator and not tags_validator.is_valid:
+        if len(tags.selected) == 0:
             return
-
-        all_tags = set(tags.value.split(" "))
-
-        async with db_session() as db:
-            tag_results = (
-                await db.exec(
-                    select(MessageTag).where(col(MessageTag.name).in_(all_tags))
-                )
-            ).all()
-
-        existing_tags = set([t.name for t in tag_results])
-        nonexistent_tags = all_tags.difference(existing_tags)
-
-        # create missing tags
-        for t in nonexistent_tags:
-            async with db_session() as db:
-                db.add(MessageTag(name=t))
-                await db.commit()
 
         # create message
         async with db_session() as db:
@@ -155,7 +151,7 @@ class DetailsModal(ModalScreen):
             assert message.id
 
         # link tags to message
-        for t in all_tags:
+        for t in tags.selected:
             async with db_session() as db:
                 db.add(MessageTags(message_id=message.id, tag_name=t))
                 await db.commit()
