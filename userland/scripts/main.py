e970b0bfd0b809c367c957700e00e7ca03157137
haliphax <haliphax@gmail.com>
2025-11-14 16:41:55 -0600

    ✨ remember selected button in main menu

--- userland/scripts/main.py
+++ userland/scripts/main.py
@@ -13,6 +13,7 @@ from xthulu.ssh.context import SSHContext
 class MenuApp(XthuluApp):
     """Main menu"""
 
+    BINDINGS = [("escape", "quit", "Log off")]
     CSS = """
         Button {
             margin-bottom: 1;
@@ -26,16 +27,21 @@ class MenuApp(XthuluApp):
         }
     """
     """Stylesheet"""
+    _last: str | None = None
+
+    def __init__(self, context: SSHContext, last: str | None = None, **kwargs):
+        super().__init__(context, **kwargs)
+        self._last = last
 
     def compose(self) -> ComposeResult:
         yield Center(
             Middle(
                 VerticalScroll(
-                    Button("Messages", name="messages"),
-                    Button("Node chat", name="chat"),
-                    Button("Oneliners", name="oneliners"),
-                    Button("Lock example", name="lock_example"),
-                    Button("Log off", name="goto_logoff", variant="error"),
+                    Button("Messages", id="messages"),
+                    Button("Node chat", id="chat"),
+                    Button("Oneliners", id="oneliners"),
+                    Button("Lock example", id="lock_example"),
+                    Button("Log off", id="goto_logoff", variant="error"),
                 )
             )
         )
@@ -43,18 +49,28 @@ class MenuApp(XthuluApp):
         self.context.proc.stdout.write(b"\x1b[?1049l")
 
     async def on_button_pressed(self, event: Button.Pressed) -> None:
-        assert event.button.name
+        assert event.button.id
+        return self.exit(result=event.button.id)
 
-        return self.exit(result=event.button.name)
+    async def action_quit(self) -> None:
+        self.context.console.clear()
+        self.context.goto("logoff")
+
+    async def on_ready(self) -> None:
+        if self._last:
+            btn = self.get_widget_by_id(self._last)
+            btn.focus()
 
 
 async def main(cx: SSHContext) -> None:
+    result: str | None = None
+
     while True:
         cx.console.set_window_title("main menu")
-        result: str | None = await MenuApp(cx).run_async()
+        result = await MenuApp(cx, result).run_async()
 
         if not result:
-            return
+            result = "goto_logoff"
 
         if result.startswith("goto_"):
             cx.console.clear()
