a821a38b493cdf28c97347cc79f8cc2ce8c0f4ba
haliphax <haliphax@gmail.com>
2025-11-16 00:37:08 -0600

    ✨ header for message view screen

--- userland/scripts/messages/view_screen.py
+++ userland/scripts/messages/view_screen.py
@@ -1,8 +1,12 @@
 """Message viewer screen"""
 
+# stdlib
+from typing import Sequence
+
 # 3rd party
+from textual.containers import Horizontal, Vertical
 from textual.screen import Screen
-from textual.widgets import Footer, MarkdownViewer
+from textual.widgets import Footer, Label, MarkdownViewer
 
 # local
 from userland.models import Message
@@ -12,15 +16,53 @@ class ViewScreen(Screen):
     """Message viewer screen"""
 
     BINDINGS = [("escape", "app.pop_screen", "Exit")]
+    CSS = """
+        Label {
+            width: 50%;
+        }
+
+        MarkdownViewer {
+            padding-top: 1;
+        }
+
+        #header {
+            background: #700;
+            height: 4;
+            padding-left: 1;
+            padding-right: 1;
+            padding-top: 1;
+        }
+    """
 
     message: Message
+    tags: Sequence[str]
 
-    def __init__(self, *args, message: Message, **kwargs):
+    def __init__(self, *args, message: Message, tags: Sequence[str], **kwargs):
         super().__init__(*args, **kwargs)
         self.message = message
+        self.tags = tags
 
     def compose(self):
-        yield MarkdownViewer(
-            markdown=self.message.content, show_table_of_contents=False
+        assert self.message.author
+        yield Vertical(
+            Vertical(
+                Horizontal(
+                    Label(
+                        f"[bold underline]Author:[/] {self.message.author.name}"
+                    ),
+                    Label(
+                        f"[bold underline]Posted:[/] {self.message.created.strftime('%H:%M %a %b %d %Y')}"
+                    ),
+                ),
+                Horizontal(
+                    Label(f"[bold underline]Title:[/]  {self.message.title}"),
+                    Label(f"[bold underline]Tags:[/]   {', '.join(self.tags)}"),
+                ),
+                id="header",
+            ),
+            MarkdownViewer(
+                markdown=self.message.content,
+                show_table_of_contents=False,
+            ),
+            Footer(),
         )
-        yield Footer()
